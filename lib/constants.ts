8638ee7b2295a5d482b0a1b154549c61a8eb7497
Bishal Neupane <Neupanebishal07@gmail.com>
2023-03-23 10:31:33 +0545

    Deploy url update

--- lib/constants.ts
+++ lib/constants.ts
@@ -15,4 +15,4 @@ export const FADE_UP_ANIMATION_VARIANTS = {
 };
 
 export const DEPLOY_URL =
-  "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FBishalN%2FThreadgenie&env=OPENAI_API_KEY,UPSTASH_REDIS_REST_URL,UPSTASH_REDIS_REST_TOKEN&project-name=threadgenie&repository-name=threadgenie&demo-title=The%20Ultimate%20Tool%20for%20Crafting%20Twitter%20Threads&demo-description=Effortlessly%20Create%20Engaging%20and%20Informative%20Threads%20in%20Minutes&demo-url=https%3A%2F%2Fthreadgenie.vercel.app%2F";
+  "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FBishalN%2Fbrandify&env=OPENAI_API_KEY";
